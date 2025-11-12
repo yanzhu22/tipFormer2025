cdc492f87f5386762ed54b17469e7a28d61ab84a
yanzhu <95071368+yanzhu22@users.noreply.github.com>
2025-11-12 21:18:42 +0800

    Refactor feature extraction methods for pollutants and proteins
    Refactored feature extraction to include compound and protein feature calculations.

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -1,21 +1,99 @@
-from transformers import AutoTokenizer, AutoModel
+import os
+import sys
+sys.path.append('./dataset/BerMol/')
 import torch
+import pickle
+import dill as pickle
+import json
+import esm
+import pandas as pd
+from bermol.trainer import BerMolPreTrainer
+from tqdm import tqdm
+from collections import OrderedDict
 
-class FeatureExtractor:
-    def __init__(self, chem_model_name="seyonec/PubChem10M_SMILES_BPE_450k", protein_model_name="Rostlab/prot_bert"):
-        self.chem_tokenizer = AutoTokenizer.from_pretrained(chem_model_name)
-        self.chem_model = AutoModel.from_pretrained(chem_model_name)
-        self.protein_tokenizer = AutoTokenizer.from_pretrained(protein_model_name)
-        self.protein_model = AutoModel.from_pretrained(protein_model_name)
 
-    def extract_chem_features(self, smiles):
-        inputs = self.chem_tokenizer(smiles, padding='max_length', truncation=True, max_length=512, return_tensors='pt')
+def cal_comp_feat(data: pd.DataFrame, model_path: str, device: str = "cuda") -> dict:
+    """
+    Calculate the compound features using the compound pre-trained model
+    """
+    with open(model_path, "rb") as f:
+        comp_model = pickle.load(f)
+        comp_model.model.to(device)
+        comp_model.model.eval()
+
+    def smi_to_vec(smi):
+        output = comp_model.transform(smi, device)
+        return output[1].cpu().detach().numpy().reshape(-1)
+
+    comp_feat = {}
+    comp_data = data[["ChemicalName", "SMILES"]].drop_duplicates(subset=["ChemicalName"])
+    
+    for _, row in tqdm(comp_data.iterrows()):
+        cid, smi = row[0], row[1]
+        print(smi)
+        comp_feat[cid] = smi_to_vec(smi)
+        
+
+    return comp_feat
+
+
+def cal_prot_feat(data: pd.DataFrame) -> dict:
+    """
+    Calculate the protein features using the protein pre-trained model
+    """
+    # model, alphabet = esm.pretrained.esm2_t30_150M_UR50D()
+    model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+    model = model.cuda()
+    batch_converter = alphabet.get_batch_converter()
+    model.eval()
+    repr_layer = model.num_layers
+
+    def seq_to_vecs(pid, seq, max_length=1022):
+        data = [
+            (pid, seq[:max_length]),
+        ]
+        _, _, batch_tokens = batch_converter(data)
+        batch_tokens = batch_tokens.to(device="cuda", non_blocking=True)
+
         with torch.no_grad():
-            outputs = self.chem_model(**inputs)
-        return outputs.last_hidden_state
+            results = model(batch_tokens, repr_layers=[repr_layer])
+        token_representations = results["representations"][repr_layer]
+        sequence_representations = token_representations[0, 1:].mean(0)
+        return sequence_representations.cpu().detach().numpy().reshape(-1)
 
-    def extract_protein_features(self, protein_seq):
-        inputs = self.protein_tokenizer(protein_seq, padding='max_length', truncation=True, max_length=1024, return_tensors='pt')
-        with torch.no_grad():
-            outputs = self.protein_model(**inputs)
-        return outputs.last_hidden_state
+    prot_feat = {}
+    prot_data = data[["TargetUniProtID", "Sequence"]].drop_duplicates(subset=["TargetUniProtID"])
+    for _, row in tqdm(prot_data.iterrows()):
+        pid, seq = row[0], row[1]
+        prot_feat[pid] = seq_to_vecs(pid, seq)
+
+    return prot_feat
+
+def extract_dti() -> None:
+    
+    save_path = "./feature"
+    os.makedirs(save_path, exist_ok=True)
+
+    #drug_smi = pd.read_csv("./dataset/pollutants.csv", sep=",")
+    drug_smi = pd.read_csv("./dataset/pollutants.csv", sep=",", usecols=['ChemicalName', 'SMILES'])
+
+    tar_seq = pd.read_csv("./dataset/proteins.csv", sep=",", usecols=['TargetUniProtID', 'Sequence'])
+    #tar_seq = pd.read_csv("./uniprotkb_Human_AND_model_organism_9606_2025_07_20.tsv", sep="\t", usecols=['TargetUniProtID', 'Sequence'])
+
+    #drug_smi.columns = ["ChemicalName", "SMILES"]
+    #tar_seq.columns = ["TargetUniProtID", "Sequence"]
+
+    print(f"Extracting compound features ...")
+    comp_feat = cal_comp_feat(drug_smi, bermol_model_path)
+    with open(save_path + "compound_features_DTIAM.pkl", "wb") as f:
+        pickle.dump(comp_feat, f)
+
+    print(f"Extracting protein features ...")
+    prot_feat = cal_prot_feat(tar_seq)
+    with open(save_path + "protein_features_DTIAM.pkl", "wb") as f:
+        pickle.dump(prot_feat, f)
+
+
+if __name__ == "__main__":
+    bermol_model_path = "./BerMolModel_base.pkl"
+    extract_dti()
