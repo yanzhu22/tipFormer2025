63333afb775671ff4d5a418a0eba16e2d81e03fa
yanzhu <21B903035@stu.hit.edu.cn>
2025-11-13 09:21:17 +0800

    Add BerMol

---
+++ model/BerMol/scripts/featurize.py
@@ -0,0 +1,17 @@
+from bermol.trainer import BerMolPreTrainer
+import numpy as np
+
+
+path = ""
+predictor = BerMolPreTrainer.load(path)
+
+
+def smi_feature(smi: str) -> np.ndarray:
+    output = predictor.transform([smi])
+    pooled_output = output[0][1]
+    return pooled_output.cpu().detach().numpy().reshape(-1)
+
+
+smi = "Cc1nc2c(c(-c3ccc(Cl)cc3Cl)c1CN)C(=O)N(CC(=O)N1CCCC1)C2"
+vec = smi_feature(smi)
+print(vec)
